986980b08c9caf9dc189dc3b234794f6e4ea31c8
whymatter <seitz-oliver@gmx.de>
2019-08-12 21:58:09 +0200

    environment callback

--- ros/src/roadmodel/include/roadmodel/roadmodel_node.h
+++ ros/src/roadmodel/include/roadmodel/roadmodel_node.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include <boost/bind.hpp>
+#include <alb_ros_msgs/CamDetections.h>
+#include <alb_ros_msgs/StaticEnvironment.h>
 
 #include "alb_base/alb_node.h"
 
@@ -15,6 +17,12 @@ namespace alb {
   public:
       explicit RoadmodelNode(TNode &nodeHandle)
               : alb::base::AlbNode<TNode>(nodeHandle) {
+          ::boost::function<void(const ::alb_ros_msgs::StaticEnvironmentConstPtr &)> boundCallback = ::boost::bind(
+                  &RoadmodelNode::EnvironmentCallback, this, _1);
+          this->template Subscribe<::alb_ros_msgs::StaticEnvironment>("/static_environment/cups", boundCallback);
+      }
+
+      void EnvironmentCallback(const ::alb_ros_msgs::StaticEnvironmentConstPtr &ros) {
 
       }
 
